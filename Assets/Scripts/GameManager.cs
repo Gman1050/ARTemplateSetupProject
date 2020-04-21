cb13c953039b04eafc4593090dc7780c6cf40add
Ryan Zagala <gman10501@gmail.com>
2020-04-21 11:10:45 -0700

    Initial Commit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public bool debugComponent = false;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClearTouchInstantiatedObjects()
+    {
+        var allGameObjects = FindObjectsOfType<GameObject>();
+
+        for (int i = 0; i < allGameObjects.Length; i++)
+        {
+            if (debugComponent)
+                Debug.Log("allGameObjects[i]: " + allGameObjects[i].name);
+
+            if (allGameObjects[i].name.Contains("Clone"))
+                Destroy(allGameObjects[i]);
+            
+        }
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ImageRecogintion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecogintion : MonoBehaviour
+{
+    private ARTrackedImageManager arTrackedImageManager;
+
+    void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChange;
+    }
+
+    void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChange;
+    }
+
+    void Awake()
+    {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnImageChange(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log("trackedImage.name: " + trackedImage.name);
+            
+        }
+    }
+}
